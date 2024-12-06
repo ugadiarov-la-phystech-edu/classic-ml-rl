069cdc4076059dce9e7322544a9deb4685f1b677
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2024-12-06 05:09:23 +0300

    Added multiflow algorithms

---
+++ catch.py
@@ -0,0 +1,388 @@
+#@title Implementation
+
+# %load catch.py
+# pylint: disable=g-bad-file-header
+# Copyright 2019 DeepMind Technologies Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Catch reinforcement learning environment."""
+
+import gym
+import numpy as np
+import pygame
+import sys
+import os
+def is_notebook():
+    try:
+        get_ipython
+        return True
+    except NameError:
+        return False
+# colab_rendering = "google.colab" in sys.modules
+# if colab_rendering or is_notebook():
+#     # set SDL to use the dummy NULL video driver,
+#     #   so it doesn't need a windowing system.
+#     from google.colab import output
+#     os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+_ACTIONS = (-1, 0, 1)  # Left, no-op, right.
+ACTION_LEFT = -1
+ACTION_NOOP = 0
+ACTION_RIGHT = 1
+
+OBSERVATION_TYPE_DEFAULT = 0
+OBSERVATION_TYPE_MINIMAL = 1
+OBSERVATION_TYPE_DEFAULT_WITH_COLORS = 2
+OBSERVATION_TYPE_DEFAULT_ONEHOT = 3
+OBSERVATION_TYPES = [
+    OBSERVATION_TYPE_DEFAULT,
+    OBSERVATION_TYPE_MINIMAL,
+    OBSERVATION_TYPE_DEFAULT_WITH_COLORS,
+    OBSERVATION_TYPE_DEFAULT_ONEHOT,
+]
+OBSERVATION_TYPE_3D = 4
+OBSERVATION_TYPE_3D_GRID = 5
+
+# Define colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+grey = (128, 128, 128)
+red = (255, 0, 0)
+yellow = (255, 255, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+light_blue = (154, 187, 255)
+dark_grey = (110,110,110)
+ligth_grey = (235,235,235)
+dark_blue = (35, 110, 150)
+
+
+class Catch(gym.Env):
+    metadata = {
+        "render_modes": ["inline", "terminal", "graphic", "notebook"],
+        "render_fps": 50,
+    }
+    """A Catch environment built on the dm_env.Environment class.
+
+  The agent must move a paddle to intercept falling balls. Falling balls only
+  move downwards on the column they are in.
+
+  The observation is an array shape (rows, columns), with binary values:
+  zero if a space is empty; 1 if it contains the paddle or a ball.
+
+  The actions are discrete, and by default there are three available:
+  stay, move left, and move right.
+
+  The episode terminates when the ball reaches the bottom of the screen.
+  """
+
+    def __init__(self, rows: int = 10, columns: int = 5, seed=None, observation_type=0, render_mode="inline"):
+        """Initializes a new Catch environment.
+
+        Args:
+          rows: number of rows.
+          columns: number of columns.
+          seed: random seed for the RNG.
+        """
+        self._rows = rows
+        self._columns = columns
+        self.width = columns
+        self.height = rows
+        self._rng = np.random.RandomState(seed)
+        self._board = np.zeros((rows, columns), dtype=np.float32)
+        self._ball_x = None
+        self._ball_y = None
+        self._paddle_x = None
+        self._paddle_y = None
+        self._reset_next_step = True
+        self._total_regret = 0.0
+        self.observation_type = observation_type
+        self.observation_space = self.observation_spec(observation_type)
+        self.action_space = self.action_spec()
+        # Rendering
+        self.pygame_initialized = False
+        self.render_mode = render_mode
+
+    def reset(self):
+        """Returns the first `TimeStep` of a new episode."""
+        self._reset_next_step = False
+        self._ball_x = self._rng.randint(self._columns)
+        self._ball_y = 0
+        self._paddle_x = self._columns // 2
+        self._paddle_y = self._rows - 1
+        self.observation_space = self.observation_spec(self.observation_type)
+        self.action_space = self.action_spec()
+        return self._observation()
+
+    def step(self, action: int):
+        """Updates the environment according to the action."""
+        if self._reset_next_step:
+            return self.reset()
+
+        # Move the paddle.
+        dx = _ACTIONS[action]
+        self._paddle_x = np.clip(self._paddle_x + dx, 0, self._columns - 1)
+
+        # Drop the ball.
+        self._ball_y += 1
+
+        # Check for termination.
+        if self._ball_y == self._paddle_y:
+            reward = 1.0 if self._paddle_x == self._ball_x else -1.0
+            self._reset_next_step = True
+            self._total_regret += 1.0 - reward
+            return self._observation(), reward, True, {}
+
+        return self._observation(), 0.0, False, {}
+
+    def observation_spec(self, observation_type):
+        """Returns the observation spec."""
+        if observation_type == OBSERVATION_TYPE_MINIMAL:
+            highest_num = max(self._columns, self._rows)
+            return gym.spaces.Box(
+                low=-highest_num, high=highest_num, shape=(4,), dtype=np.int32
+            )
+        if observation_type == OBSERVATION_TYPE_DEFAULT_WITH_COLORS:
+            return gym.spaces.Box(
+                low=0.0,
+                high=255.0,
+                shape=(self._rows, self._columns, 3),
+                dtype=np.float32,
+            )
+        if observation_type == OBSERVATION_TYPE_DEFAULT_ONEHOT:
+            return gym.spaces.Box(
+                low=0.0, high=1.0, shape=(self._rows, self._columns, 2), dtype=np.int32
+            )
+        if observation_type == 4:
+            return gym.spaces.Box(
+                low=0.0,
+                high=1.0,
+                shape=(self._rows, self._columns, self._rows),
+                dtype=np.float32,
+            )
+        return gym.spaces.Box(
+            low=0.0, high=1.0, shape=self._board.shape, dtype=np.bool_
+        )
+
+    def action_spec(self):
+        """Returns the action spec."""
+        return gym.spaces.Discrete(len(_ACTIONS))
+
+    def _observation(self):
+        if self.observation_type == OBSERVATION_TYPE_MINIMAL:
+            return np.array(
+                [self._paddle_x, self._paddle_y, self._ball_x, self._ball_y]
+            )
+        if self.observation_type == OBSERVATION_TYPE_DEFAULT_WITH_COLORS:
+            a = np.zeros((self._rows, self._columns, 3))
+            a[self._ball_y, self._ball_x, 0] = 128.0
+            a[self._ball_y, self._ball_x, 1] = 128.0
+            a[self._ball_y, self._ball_x, 2] = 128.0
+            a[self._paddle_y, self._paddle_x, 0] = 255.0
+            a[self._paddle_y, self._paddle_x, 1] = 255.0
+            a[self._paddle_y, self._paddle_x, 2] = 255
+            return a
+        if self.observation_type == OBSERVATION_TYPE_DEFAULT_ONEHOT:
+            a = np.zeros((self._rows, self._columns, 2))
+            a[self._ball_y, self._ball_x, 0] = 1.0
+            a[self._paddle_y, self._paddle_x, 1] = 1.0
+            return a
+        self._board.fill(0.0)
+        self._board[self._ball_y, self._ball_x] = 1.0
+        self._board[self._paddle_y, self._paddle_x] = 1.0
+        return self._board.copy()
+
+    def render(self, mode=None):
+        if mode is None:
+            mode = self.render_mode
+        if mode == "graphic" or mode == "notebook":
+            # Initialize pygame
+            if not self.pygame_initialized:
+                pygame.init()
+                self.cell_size = 50
+                self.screen_width, self.screen_height = (
+                    self.width * self.cell_size,
+                    (self.height) * self.cell_size,
+                )
+                self.screen = pygame.display.set_mode([self.screen_width, self.screen_height])
+                pygame.display.set_caption("Catch Environment")
+                self.pygame_initialized = True
+
+            # Set background color
+            self.screen.fill(dark_blue)
+            grid_width = 5
+            # Draw white bg
+            pygame.draw.rect(self.screen, ligth_grey, pygame.Rect(
+                grid_width,
+                grid_width,
+                self.screen_width - 10,
+                self.screen_height - 10,
+            ))
+
+            # Draw Ball
+            blue = (35, 110, 150)
+            yellow = (243, 188, 87)
+            red = (156,39,6)
+            grey = (235,235,235)
+
+            light_yellow = (243, 188, 87)
+            ball_radius = ((self.cell_size - grid_width*4) // 2)
+            pygame.draw.circle(self.screen, light_yellow,(
+                (self._ball_x * self.cell_size) + grid_width*2 + ball_radius,
+                (max(0, (self._ball_y)) * self.cell_size) + grid_width*3 + ball_radius),
+                ball_radius,
+            )
+            # Draw Paddle
+            # pygame.draw.rect(self.screen, green, pygame.Rect(
+            #     (self._paddle_x * self.cell_size) + grid_width,
+            #     ((self._paddle_y) * self.cell_size + (self.cell_size/2)) + grid_width,
+            #     self.cell_size  - grid_width*2,
+            #     (self.cell_size / 2) - grid_width*2,
+            # ))
+            paddle_color = (156,39,6)
+            x_pos = (self._paddle_x * self.cell_size) + grid_width
+            y_pos = ((self._paddle_y) * self.cell_size + (self.cell_size/2)) + grid_width
+            width = self.cell_size  - grid_width*2
+            height = (self.cell_size / 2) - grid_width*2
+            pygame.draw.polygon(self.screen, paddle_color, [
+                (x_pos, y_pos),
+                (x_pos + width/10, y_pos),
+                (x_pos + width/10, y_pos + (height - (height / 10))),
+                (x_pos + (width -  width/10), y_pos + (height - (height / 10))),
+                (x_pos + (width -  width/10), y_pos),
+                (x_pos + width, y_pos),
+                (x_pos + width, y_pos + height),
+                (x_pos, y_pos + height),
+                (x_pos, y_pos),
+            ], 5)
+
+            # Flip the display
+            pygame.display.flip()
+            if is_notebook():
+                from IPython.display import Image, display
+                output.clear()
+                pygame.image.save(self.screen, 'frame.png')
+                display(Image(filename='frame.png'))
+            else:
+                # Wait for a short time to slow down the rendering
+                pygame.time.wait(25)
+        else:
+            render_grid = self._observation()
+            print("\n")
+            for y, row_data in enumerate(render_grid):
+                row = ""
+                for x, cell_entry in enumerate(row_data):
+                    if cell_entry == 1:
+                        if y == 9:
+                            row += "_"
+                        else:
+                            row += "O"
+                    else:
+                        row += " "
+                print(row)
+            print("\n")
+
+
+gym.register(
+    id="Catch-v0",
+    entry_point="catch:Catch",
+    kwargs={},
+)
+
+gym.register(
+    id="Catch-vectorized-v0",
+    entry_point="catch:Catch",
+    kwargs={"observation_type": OBSERVATION_TYPE_MINIMAL},
+)
+
+gym.register(
+    id="Catch-color-v0",
+    entry_point="catch:Catch",
+    kwargs={"observation_type": OBSERVATION_TYPE_DEFAULT_WITH_COLORS},
+)
+
+gym.register(
+    id="Catch-onehot-v0",
+    entry_point="catch:Catch",
+    kwargs={"observation_type": OBSERVATION_TYPE_DEFAULT_ONEHOT},
+)
+
+for size in range(2, 9):
+    gym.register(
+        id=f"Catch-{size}-v0",
+        entry_point="catch:Catch",
+        kwargs={"rows": 10 * size, "columns": 5 * size},
+    )
+    gym.register(
+        id=f"Catch-{size}-vectorized-v0",
+        entry_point="catch:Catch",
+        kwargs={
+            "observation_type": OBSERVATION_TYPE_MINIMAL,
+            "rows": 10 * size,
+            "columns": 5 * size,
+        },
+    )
+    gym.register(
+        id=f"Catch-{size}-color-v0",
+        entry_point="catch:Catch",
+        kwargs={
+            "observation_type": OBSERVATION_TYPE_DEFAULT_WITH_COLORS,
+            "rows": 10 * size,
+            "columns": 5 * size,
+        },
+    )
+    gym.register(
+        id=f"Catch-{size}-onehot-v0",
+        entry_point="catch:Catch",
+        kwargs={
+            "observation_type": OBSERVATION_TYPE_DEFAULT_ONEHOT,
+            "rows": 10 * size,
+            "columns": 5 * size,
+        },
+    )
+
+
+for size in range(2, 21):
+    gym.register(
+        id=f"Catch-{size}x{size}-v0",
+        entry_point="catch:Catch",
+        kwargs={"rows": size, "columns": size},
+    )
+    gym.register(
+        id=f"Catch-{size}x{size}-vectorized-v0",
+        entry_point="catch:Catch",
+        kwargs={
+            "observation_type": OBSERVATION_TYPE_MINIMAL,
+            "rows": size,
+            "columns": size,
+        },
+    )
+    gym.register(
+        id=f"Catch-{size}x{size}-color-v0",
+        entry_point="catch:Catch",
+        kwargs={
+            "observation_type": OBSERVATION_TYPE_DEFAULT_WITH_COLORS,
+            "rows": size,
+            "columns": size,
+        },
+    )
+    gym.register(
+        id=f"Catch-{size}x{size}-onehot-v0",
+        entry_point="catch:Catch",
+        kwargs={
+            "observation_type": OBSERVATION_TYPE_DEFAULT_ONEHOT,
+            "rows": size,
+            "columns": size,
+        },
+    )
---
+++ multiflow_dqn.py
@@ -0,0 +1,275 @@
+import argparse
+import copy
+import importlib
+import traceback
+
+import gym
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import numpy as np
+import torch
+from click.core import batch
+
+import wandb
+
+from skmultiflow.trees import HoeffdingTreeRegressor, HoeffdingAdaptiveTreeRegressor
+from skmultiflow.lazy import KNNRegressor
+
+import catch
+
+
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+# if GPU is to be used
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
+device = 'cpu'
+
+
+class RFQ:
+    def __init__(self, cls, n_actions, **rf_kwargs):
+        self.cls = cls
+        self.kwargs = rf_kwargs
+        self.n_actions = n_actions
+        self.rf_regressor = None
+
+    def q_value(self, state_action):
+        return self.rf_regressor.predict(state_action)
+
+    def _q_values(self, state):
+        state_action = np.stack(
+            [np.concatenate((state, np.full((state.shape[0], 1), fill_value=action, dtype=state.dtype)), axis=-1) for
+             action in range(self.n_actions)], axis=1)
+        return self.q_value(state_action.reshape((-1, state_action.shape[-1]))).reshape(
+            (state.shape[0], self.n_actions))
+
+    def act(self, state):
+        return np.argmax(self._q_values(state), axis=-1)
+
+    def value(self, state):
+        return np.max(self._q_values(state), axis=-1)
+
+    def train(self, state_action, q_value, init=False):
+        if init:
+            assert self.rf_regressor is None
+            self.rf_regressor = self.cls(**self.kwargs)
+            return self.rf_regressor.fit(state_action, q_value)
+        else:
+            return self.rf_regressor.partial_fit(state_action, q_value)
+
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def get_data(self):
+        return list(self.memory)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+def plot_metric(metric_history, show_result=False):
+    plt.figure(1)
+    metric_history_tensor = torch.tensor(metric_history, dtype=torch.float)
+    if show_result:
+        plt.title('Result')
+    else:
+        plt.clf()
+        plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(metric_history_tensor.numpy())
+    # Take 100 episode averages and plot them too
+    if len(metric_history_tensor) >= 100:
+        means = metric_history_tensor.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    if is_ipython:
+        if not show_result:
+            display.display(plt.gcf())
+            display.clear_output(wait=True)
+        else:
+            display.display(plt.gcf())
+
+
+def select_action(state, rfq, steps_done, total_timesteps, eps_start, eps_end, exploration_fraction):
+    sample = random.random()
+    eps_threshold = eps_end
+    if steps_done <= exploration_fraction * total_timesteps:
+        eps_threshold = eps_start - steps_done / total_timesteps / exploration_fraction * (eps_start - eps_end)
+
+    if sample > eps_threshold and rfq.rf_regressor is not None:
+        action = rfq.act(state)[np.newaxis]
+    else:
+        action = np.full((1, 1), fill_value=env.action_space.sample(), dtype=np.int64)
+
+    return torch.as_tensor(action, device=device, dtype=torch.int64)
+
+
+def optimize_model(rfq, target_rfq, buffer, batch_size, gamma, init=False):
+    if init:
+        transitions = buffer.get_data()
+    else:
+        transitions = buffer.sample(batch_size)
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool).numpy()
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None]).numpy()
+    state_batch = torch.cat(batch.state).numpy()
+    action_batch = torch.cat(batch.action).numpy()
+    reward_batch = torch.cat(batch.reward).numpy()
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1).values
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = np.zeros((state_batch.shape[0],), dtype=np.float32)
+    if target_rfq.rf_regressor is not None:
+        next_state_values[non_final_mask] = target_rfq.value(non_final_next_states)
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * gamma) + reward_batch
+    rfq.train(np.concatenate([state_batch, action_batch], axis=-1), expected_state_action_values, init=init)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cls', type=str, required=True)
+    args = parser.parse_args()
+    cls = None
+    for module in ["skmultiflow.trees", "skmultiflow.lazy"]:
+        try:
+            cls = getattr(importlib.import_module(module), args.cls)
+        except Exception:
+            print(traceback.format_exc())
+
+    if cls is None:
+        raise ValueError(f'Cannot find class by name: {args.cls}')
+
+
+    # CartPole-v1
+    # BATCH_SIZE = 128
+    # GAMMA = 0.99
+    # EPS_START = 0.9
+    # EPS_END = 0.05
+    # EXPLORATION_FRACTION = 1.0
+    # TAU = 0.005
+    # LR = 1e-4
+    # BUFFER_SIZE = 10000
+    # TRAINING_STARTS = 5000
+    # TOTAL_TIMESTEPS = 20000
+    # TRAIN_FREQ = 1
+
+    # env = gym.make("CartPole-v1")
+
+
+    BATCH_SIZE = 32
+    GAMMA = 0.99
+    EPS_START = 1
+    EPS_END = 0.05
+    EXPLORATION_FRACTION = 0.35
+    TARGET_UPDATE_INTERVAL = 100
+    LR = 1e-3
+    BUFFER_SIZE = 1000
+    TRAINING_STARTS = 500
+    TOTAL_TIMESTEPS = 20000
+    TRAIN_FREQ = 1
+
+    env_id = 'Catch-vectorized-v0'
+    env = gym.make(env_id)
+
+    # Get number of actions from gym action space
+    n_actions = env.action_space.n
+    # Get the number of state observations
+    state = env.reset()
+
+    # kwargs = {'grace_period': BATCH_SIZE} if 'Hoeffding' in args.cls else {}
+    kwargs = {}
+    rfq = RFQ(cls, n_actions, **kwargs)
+    target_rfq = rfq
+    memory = ReplayMemory(BUFFER_SIZE)
+
+    steps_done = 0
+    num_episodes = 0
+    episode_durations = []
+    episode_return_history = []
+
+    run = wandb.init(project=env_id, group=args.cls, config=kwargs, sync_tensorboard=True, monitor_gym=True,)
+
+    while steps_done < TOTAL_TIMESTEPS:
+        # Initialize the environment and get its state
+        state = env.reset()
+        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+        episode_return = 0
+        for t in count():
+            action = select_action(state.numpy(), rfq, steps_done, TOTAL_TIMESTEPS, EPS_START, EPS_END, EXPLORATION_FRACTION)
+            observation, reward, done, _ = env.step(action.item())
+            steps_done += 1
+            episode_return += reward
+            reward = torch.tensor([reward], device=device)
+            if done:
+                next_state = None
+            else:
+                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+
+            # Store the transition in memory
+            memory.push(state, action.to(torch.float32), next_state, reward)
+
+            # Move to the next state
+            state = next_state
+
+            if steps_done >= TRAINING_STARTS and steps_done % TRAIN_FREQ == 0:
+                # Perform one step of the optimization (on the policy network)
+                optimize_model(rfq, target_rfq, memory, BATCH_SIZE, GAMMA, init=steps_done == TRAINING_STARTS)
+                if target_rfq.rf_regressor is None:
+                    target_rfq = copy.deepcopy(rfq)
+
+            if steps_done % TARGET_UPDATE_INTERVAL == 0:
+                target_rfq = copy.deepcopy(rfq)
+
+            if done:
+                episode_return_history.append(episode_return)
+                episode_return = 0
+                num_episodes += 1
+                episode_durations.append(t + 1)
+                # plot_metric(episode_durations)
+                # plot_metric(episode_return_history)
+                wandb.log({'global_step': steps_done, 'rollout/ep_len_mean': np.mean(episode_durations[-100:]),
+                           'rollout/ep_rew_mean': np.mean(episode_return_history[-100:]), 'time/episodes': num_episodes})
+                break
+
+    print('Complete')
+    # plot_metric(episode_durations, show_result=True)
+    # plot_metric(episode_return_history, show_result=True)
+    # plt.ioff()
+    # plt.show()
